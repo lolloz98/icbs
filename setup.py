80727a31ebcb29cf2227586e550aadc93b41d437
lolloz98 <lorenzocarpaneto@yahoo.it>
2020-05-30 23:36:59 +0200

    UPLOAD setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='icbs-lolloz98',
+    name='icbs',
     version='0.0.1',
     description='Simple image cut and rebuild package. It allows to decide how to cut an image in overlapping squares. After having cut the image you can rebuild it (or you can do some processing before rebuilding it)',
     long_description=long_description,
